8377b4aa5a716dc6887e483d16ac319ab8902368
0hypercube <0hypercube@gmail.com>
2021-09-09 20:33:54 +0100

    ⛰ Add height map

--- cerium/src/world.rs
+++ cerium/src/world.rs
@@ -14,7 +14,30 @@ impl Plugin for WorldPlugin {
 	}
 }
 
-fn get_mesh(order: usize, radius: f32) -> Mesh {
+fn sphere_uv(d: &Vec3A) -> Vec2 {
+	Vec2::new(
+		0.5 + f32::atan2(d.x, d.z) / (std::f32::consts::PI * 2.),
+		0.5 - d.y.asin() / std::f32::consts::PI,
+	)
+}
+
+fn height(d: &Vec3A, texture: &Texture) -> f32 {
+	let sphere_uv = sphere_uv(d);
+
+	if sphere_uv.x > 1. && sphere_uv.y > 1. {
+		error!("More than one {:?}", sphere_uv);
+	}
+	let dims = texture.size;
+	let pixel_coord =
+		(Vec2::new((dims.width - 1) as f32, (dims.height - 1) as f32) * sphere_uv).as_u32();
+
+	let pixel_index = ((dims.width) * (pixel_coord.y) + (pixel_coord.x)) as usize * 4;
+	let pixel_value_r = u8::MAX - texture.data[pixel_index];
+
+	pixel_value_r as f32 / u8::MAX as f32
+}
+
+fn get_mesh(order: usize, radius: f32, height_map: &Texture) -> Mesh {
 	let triangles = [
 		0, 1, 2, 0, 2, 3, 0, 3, 4, 0, 4, 1, 5, 2, 1, 5, 3, 2, 5, 4, 3, 5, 1, 4,
 	];
@@ -81,7 +104,9 @@ fn get_mesh(order: usize, radius: f32) -> Mesh {
 
 	let points = subdivided
 		.iter()
-		.map(|&p| (p.normalize() * radius).into())
+		.map(|&p| p.normalize())
+		.map(|p| p * (height(&p, height_map) + radius))
+		.map(|p| p.into())
 		.collect::<Vec<[f32; 3]>>();
 
 	let colour = colours
@@ -105,7 +130,7 @@ fn get_mesh(order: usize, radius: f32) -> Mesh {
 }
 
 fn setup(asset_server: Res<AssetServer>, mut commands: Commands) {
-	let texture_handle: Handle<Texture> = asset_server.load("textures/height_map.png");
+	let texture_handle: Handle<Texture> = asset_server.load("textures/test.png");
 	commands.insert_resource(WorldTexture(texture_handle));
 }
 
@@ -118,9 +143,9 @@ fn load_world(
 ) {
 	if let Some(world_texture) = world_texture {
 		if let Some(height_map) = textures.get(&world_texture.0) {
-			info!("Dims {:?}", height_map.size);
+			info!("Dims {:?}, len {}", height_map.size, height_map.data.len());
 			commands.spawn_bundle(PbrBundle {
-				mesh: meshes.add(get_mesh(8, 2.)),
+				mesh: meshes.add(get_mesh(100, 2., height_map)),
 				material: materials.add(StandardMaterial {
 					roughness: 0.7,
 					base_color: Color::rgba_u8(5, 5, 5, 255),
