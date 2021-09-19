3f645f37d46e3a143aa2bf849199d994eb893ea4
0hypercube <0hypercube@gmail.com>
2021-09-19 13:44:57 +0100

    🖥 Serializable unit position data

--- packets/src/serialize_types.rs
+++ packets/src/serialize_types.rs
@@ -1,3 +1,5 @@
+use glam::{Quat, Vec3};
+
 use crate::Serializable;
 
 use std::fmt;
@@ -53,4 +55,49 @@ impl Serializable for Authentication {
 			password: String::deserialize(buf)?,
 		})
 	}
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Unit {
+	pub start: Vec3,
+	pub end: Vec3,
+	pub time: f32,
+}
+
+impl Unit {
+	pub fn get_position(&self) -> Vec3 {
+		let start = Quat::from_scaled_axis(self.start);
+		let end = Quat::from_scaled_axis(self.end);
+		start.lerp(end, self.time).to_axis_angle().0
+	}
+}
+
+impl Serializable for Unit {
+	fn serialize(&self, buf: &mut Vec<u8>) {
+		self.start.serialize(buf);
+		self.end.serialize(buf);
+		self.time.serialize(buf);
+	}
+
+	fn deserialize(buf: &mut crate::ReadBuffer) -> Result<Self, crate::error::ReadValueError> {
+		Ok(Self {
+			start: Vec3::deserialize(buf)?,
+			end: Vec3::deserialize(buf)?,
+			time: f32::deserialize(buf)?,
+		})
+	}
+}
+
+#[test]
+fn test_get_position() {
+	let japan = Vec3::new(0.52484196, 0.5836691, -0.6195735);
+	let germany = Vec3::new(0.14106606, 0.79356587, 0.59190667);
+	let mut unit = Unit {
+		start: japan,
+		end: germany,
+		time: 0.,
+	};
+	assert_eq!(unit.get_position(), japan);
+	unit.time = 1.;
+	assert_eq!(unit.get_position(), germany);
 }
